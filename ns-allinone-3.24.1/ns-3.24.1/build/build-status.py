709cd118c8ddbe7f80598dd58bcb6fb639e165f1
Thomas <cs13p1002@iith.ac.in>
2016-06-26 10:59:57 +0530

    Energy module for public

---
+++ ns-allinone-3.24.1/ns-3.24.1/build/build-status.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+# Programs that are runnable.
+ns3_runnable_programs = ['build/scratch/subdir/ns3.24.1-subdir-debug', 'build/scratch/ns3.24.1-scratch-simulator-debug']
+
+# Scripts that are runnable.
+ns3_runnable_scripts = []
+
