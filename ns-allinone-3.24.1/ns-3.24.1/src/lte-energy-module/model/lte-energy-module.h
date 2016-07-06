03ce6e4c9a6f047a39485ca6566ddb1f006b9959
Thomas <cs13p1002@iith.ac.in>
2016-07-06 22:56:57 +0530

    Mod

---
+++ ns-allinone-3.24.1/ns-3.24.1/src/lte-energy-module/model/lte-energy-module.h
@@ -0,0 +1,12 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef LTE_ENERGY_MODULE_H
+#define LTE_ENERGY_MODULE_H
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* LTE_ENERGY_MODULE_H */
+
